df0868f9e6ee82960ff1d615841c7e516d2f23ac
VikramReddy2703 <130277413+VikramReddy2703@users.noreply.github.com>
2023-06-19 15:27:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ParagraphLength from './components/ParagraphLength';
+
+function App() {
+  return (
+    <div className="App">
+      <ParagraphLength />
+    </div>
+  );
+}
+
+export default App;
